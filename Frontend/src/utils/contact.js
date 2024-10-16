08c2458e079cd44ef1a88fc9d0bd2b74b653500c
Pooja Bhardwaj <104709618+Poojabhardwaj98766@users.noreply.github.com>
2024-10-16 08:21:38 +0530

    Update contact.js

--- Frontend/src/utils/contact.js
+++ Frontend/src/utils/contact.js
@@ -1,2 +1,2 @@
-export const USER_API_END_POINT ="twitter-clone-server-tau.vercel.app/api/v1/user"
-export const TWEET_API_END_POINT ="twitter-clone-server-tau.vercel.app/api/v1/tweet"
+export const USER_API_END_POINT ="https://twitter-clone-server-tau.vercel.app/api/v1/user"
+export const TWEET_API_END_POINT ="https://twitter-clone-server-tau.vercel.app/api/v1/tweet"
